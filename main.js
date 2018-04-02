f7fe8e5528c9849bc0a7f8848ef37b2881dae28e
jared kantrowitz <jared.kantrowitz@gmail.com>
2018-04-02 04:11:32 -0400

    parent onChange trigger, name prop, external validation w/o pattern

--- main.js
+++ main.js
@@ -1,80 +1,77 @@
-import React, {PropTypes} from 'react';
-import ReactDOM from  'react-dom';
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import classNames from 'classnames'
 
-
 export default class FloatingLabel extends React.Component {
-  static propTypes: {
-    autoComplete: PropTypes.bool,
-    errorMsg: PropTypes.string,
-    placeholder: PropTypes.string.isRequired,
-    pattern: PropTypes.any,
-    type: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    isDisabled: PropTypes.bool
-  };
   _bind(...methods) {
-    methods.map(method=>this[method]=this[method].bind(this));
+    methods.map(method => this[method] = this[method].bind(this));
   }
-  constructor (props) {
+  constructor(props) {
     super(props)
-    this.state = {hasValue: false, hasError: false};
-    this._bind('onBlur','onChange');
+    this.state = { hasValue: false, hasError: false };
+    this._bind('onBlur', 'onChange');
   }
 
-  onBlur(event) {
-    this.setState({hasValue: Boolean(event.currentTarget.value)});
+  onBlur({ currentTarget }) {
+    this.setState({ hasValue: Boolean(currentTarget.value) });
   }
 
-  onChange(event) {
-    const {pattern} = this.props;
-    this.setState({
-      hasError: !pattern.test(event.currentTarget.value),
-      hasValue: Boolean(event.currentTarget.value)
-    });
+  onChange({ currentTarget }) {
+    const { pattern, name, onChangeTrigger } = this.props;
+    if (!pattern) {
+      this.setState({
+        hasError: false,
+        hasValue: Boolean(currentTarget.value)
+      });
+    } else {
+      this.setState({
+        hasError: !pattern.test(currentTarget.value),
+        hasValue: Boolean(currentTarget.value)
+      });
+    }
+    onChangeTrigger({ [name]: currentTarget.value });
   }
 
-  render () {
-    const {autoComplete, errorMsg, id, isDisabled, pattern, placeholder, type} = this.props;
-    const {hasValue, hasError} = this.state;
+  render() {
+    const { autoComplete, errorMsg, id, isDisabled, pattern, placeholder, type, name } = this.props;
+    const { hasValue, hasError } = this.state;
 
-    const inputClasses = classNames('fl-input', {'fl-valid': hasValue && !hasError}, {'fl-invalid': hasValue && hasError});
-    const errMsgClasses = classNames({'fl-error-msg': errorMsg}, {'fl-error-show': (hasError && hasValue) && (errorMsg && pattern)});
+    const inputClasses = classNames('fl-input', { 'fl-valid': hasValue && !hasError }, { 'fl-invalid': hasValue && errorMsg });
+    const errMsgClasses = classNames({ 'fl-error-msg': errorMsg }, { 'fl-error-show': errorMsg });
 
-    return(
+    return (
       <div className='fl-input-container'>
         <input
           autoComplete={autoComplete}
           className={inputClasses}
           disabled={isDisabled}
           id={id}
+          name={name}
           onBlur={this.onBlur}
-          onChange={pattern ? this.onChange : null}
-          type={type}/>
+          onChange={this.onChange}
+          type={type} />
         <label className='fl-input-label' htmlFor={id}>{placeholder}</label>
         <span className='fl-input-bar'></span>
-        { errorMsg && <span className={errMsgClasses}>{errorMsg}</span> }
+        {errorMsg && <span className={errMsgClasses}>{errorMsg}</span>}
       </div>
     );
   }
 }
 
+FloatingLabel.propTypes = {
+  autoComplete: PropTypes.string,
+  errorMsg: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
+  pattern: PropTypes.any,
+  type: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  isDisabled: PropTypes.bool
+};
+
 FloatingLabel.defaultProps = {
-  autoComplete: false,
+  autoComplete: "off",
   type: 'text',
   isDisabled: false,
   id: 'text-box',
   placeholder: 'name'
 };
-
-
-module.exports = FloatingLabel;
-
-//TODO: remove below lines
-const target = document.getElementById('content');
-ReactDOM.render(<FloatingLabel
-  errorMsg='Full name can contain only the alphabets and space'
-  pattern={/^[a-z\s]+$/i}
-  />,
-target);
-
